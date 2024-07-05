2b37b3c8ee44853b696a73a7c0c07d25aa16dc1c
dinesh-gaire <gairedinesh132@gmail.com>
2024-07-05 21:37:24 +0545

    utils code discussion

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app  = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"})) //to take json files
+app.use(express.urlencoded({extended: true, limit: "16kb"})) // to take data from url
+app.use(express.static("public")) //to store files and folders inside public
+app.use(cookieParser())//to access, set cookies of users browser from server and perform crud operation 
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+//connectDB is asynchronous method so it returns promise
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is listening at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed !!!", err);
+})
