7682044b231e65ca51a410f433afefd2226411a6
dinesh-gaire <gairedinesh132@gmail.com>
2024-07-07 23:50:39 +0545

    register controller bugs fixed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./.env",
+});
 
 //connectDB is asynchronous method so it returns promise
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>{
-        console.log(`Server is listening at port : ${process.env.PORT}`);
-    })
-})
-.catch((err)=>{
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is listening at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
     console.log("MONGODB connection failed !!!", err);
-})
+  });
